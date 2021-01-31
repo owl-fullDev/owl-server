1c433a21acdcb761c36109fae2ad3994b5d077bb
mariosebastian <mr557778@dal.ca>
2021-01-30 20:06:39 -0400

    new api to check warehouse quantity and product

--- src/main/java/com/owl/owlserver/Controller/HeadOfficeController/HORestockShipment.java
+++ src/main/java/com/owl/owlserver/Controller/HeadOfficeController/HORestockShipment.java
@@ -99,7 +99,7 @@ public class HORestockShipment {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "There is no product with specified ID in stock in warehouse");
         }
         else if (warehouseQuantityRepository.findByProductId(productId).getInWarehouseQuantity() < quantity) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "There is not enough quantity of product with specified ID in warehouse, Requested quantity: "+quantity+" Current Quantity in warehouse: "+warehouseQuantityRepository.findByProductId(productId).getInWarehouseQuantity());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There is not enough quantity of product with specified ID in warehouse, Current Quantity in warehouse: "+warehouseQuantityRepository.findByProductId(productId).getInWarehouseQuantity());
         }
         return new ResponseEntity<>(true,HttpStatus.OK);
     }
