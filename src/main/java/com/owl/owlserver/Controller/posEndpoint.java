2537768cf807bce4fbfe735a5133403d30736921
mariosebastian <mr557778@dal.ca>
2021-03-23 13:59:11 -0300

    50% off 2nd pair

--- src/main/java/com/owl/owlserver/Controller/posEndpoint.java
+++ src/main/java/com/owl/owlserver/Controller/posEndpoint.java
@@ -170,7 +170,6 @@ public class posEndpoint {
         }
         int promotionParentSaleId = sale.get("promotionParentSaleId").asInt();
         if(promotionParentSaleId<0) {
-            promotionParentSaleId = -promotionParentSaleId;
             newSale.setPromotionParentSaleId(promotionParentSaleId);
         }
         else if(promotionParentSaleId!=0){
