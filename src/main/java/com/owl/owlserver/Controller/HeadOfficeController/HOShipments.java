c0134ce475bd1f73c13eb6859a17b22f9d381757
mariosebastian <mr557778@dal.ca>
2021-04-25 16:49:35 -0300

    fixes to cancel shipment functionality

--- src/main/java/com/owl/owlserver/Controller/HeadOfficeController/HOShipments.java
+++ src/main/java/com/owl/owlserver/Controller/HeadOfficeController/HOShipments.java
@@ -122,8 +122,9 @@ public class HOShipments {
     @Transactional
     @PostMapping(value = "/cancelShipment")
     public ResponseEntity<String> cancelShipment(int shipmentId) {
-        shipmentRepository.findById(shipmentId).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "No shipment with ID of: "+shipmentId+" exists!"));
-        shipmentRepository.deleteById(shipmentId);
+        Shipment shipment = shipmentRepository.findById(shipmentId).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND, "No shipment with ID of: "+shipmentId+" exists!"));
+        shipmentDetailRepository.deleteInBatch(shipment.getShipmentDetailList());
+        shipmentRepository.delete(shipment);
         return new ResponseEntity<>("Successfully deleted shipment, ID: "+shipmentId, HttpStatus.OK);
     }
 }
