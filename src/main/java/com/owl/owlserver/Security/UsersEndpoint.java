a424739bd3b8fe717aa537c8282457fd96e3dea3
mariosebastian <mr557778@dal.ca>
2021-05-03 23:47:39 -0300

    users functionality

--- src/main/java/com/owl/owlserver/Security/UsersEndpoint.java
+++ src/main/java/com/owl/owlserver/Security/UsersEndpoint.java
@@ -6,8 +6,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -25,6 +29,12 @@ public class UsersEndpoint {
     @ResponseStatus(HttpStatus.OK)
     public String ping() {
         return "This is the users endpoint, get request received";
+    }
+
+    @PreAuthorize("hasRole('admin') or hasRole('boss')")
+    @GetMapping("/getAllUsers")
+    public List<UserCredentials> getAllUsers(){
+        return userCredentialsRepository.findAll();
     }
 
     @PostMapping("/login")
@@ -45,8 +55,83 @@ public class UsersEndpoint {
         return new ResponseEntity<>("Wrong username or password",HttpStatus.UNAUTHORIZED);
     }
 
+    @PreAuthorize("hasRole('admin') or hasRole('boss')")
     @PostMapping(value = "/newUser")
     public ResponseEntity<String> newUser(@RequestBody String jsonString) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode wholeJSON = objectMapper.readTree(jsonString);
+
+        String username = wholeJSON.get("username").asText();
+        String password = wholeJSON.get("password").asText();
+        String role = wholeJSON.get("role").asText();
+
+        if (role.equals("store")||role.equals("warehouse")||role.equals("office")){
+            String encodedPassword = passwordEncoder.encode(password);
+            UserCredentials userCredentials = new UserCredentials(username, encodedPassword, role);
+            userCredentialsRepository.save(userCredentials);
+            return new ResponseEntity<>("New user successfully created", HttpStatus.OK);
+        }
+        else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Role of "+role+" not recognized");
+        }
+    }
+
+    @PreAuthorize("hasRole('admin') or hasRole('boss')")
+    @PostMapping(value = "/deleteUser")
+    public ResponseEntity<String> deleteUser(@RequestBody String jsonString) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode wholeJSON = objectMapper.readTree(jsonString);
+
+        String username = wholeJSON.get("username").asText();
+        String password = wholeJSON.get("password").asText();
+
+        UserCredentials userCredentials = userCredentialsRepository.findByUsername(username);
+
+        if (userCredentials==null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"User with username of "+username+" not found");
+        }
+        String role = userCredentials.getRole();
+        if (role.equals("admin")||role.equals("boss")){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"not enough pauthorithy to delete user with role of: "+role);
+        }
+        if (!passwordEncoder.matches(password,userCredentials.getPassword())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Wrong password of deleted user given");
+        }
+        userCredentialsRepository.deleteById(userCredentials.getUserId());
+
+        return new ResponseEntity<>("User successfully deleted", HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('admin') or hasRole('boss')")
+    @PostMapping(value = "/modifyUser")
+    public ResponseEntity<String> modifyUser(@RequestBody String jsonString) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode wholeJSON = objectMapper.readTree(jsonString);
+
+        String username = wholeJSON.get("username").asText();
+        String password = wholeJSON.get("password").asText();
+        String role = wholeJSON.get("role").asText();
+
+        UserCredentials userCredentials = userCredentialsRepository.findByUsername(username);
+        if (userCredentials==null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"User with username of "+username+" not found");
+        }
+
+        if (role.equals("store")||role.equals("warehouse")||role.equals("office")||role.equals("admin")){
+            String encodedPassword = passwordEncoder.encode(password);
+            userCredentials.setPassword(encodedPassword);
+            userCredentials.setRole(role);
+            userCredentialsRepository.save(userCredentials);
+            return new ResponseEntity<>("User successfully modified", HttpStatus.OK);
+        }
+        else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Role of "+role+" not recognized");
+        }
+    }
+
+    @PreAuthorize("hasRole('boss')")
+    @PostMapping(value = "/newAdminUser")
+    public ResponseEntity<String> newAdminUser(@RequestBody String jsonString) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode wholeJSON = objectMapper.readTree(jsonString);
 
@@ -57,8 +142,23 @@ public class UsersEndpoint {
         String encodedPassword = passwordEncoder.encode(password);
         UserCredentials userCredentials = new UserCredentials(username, encodedPassword, role);
         userCredentialsRepository.save(userCredentials);
+        return new ResponseEntity<>("New user successfully created", HttpStatus.OK);
+    }
 
-        return new ResponseEntity<>("New user sucessfully created", HttpStatus.OK);
+    @PreAuthorize("hasRole('boss')")
+    @PostMapping(value = "/deleteAdminUser")
+    public ResponseEntity<String> deleteAdminUser(@RequestBody String jsonString) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode wholeJSON = objectMapper.readTree(jsonString);
+
+        String username = wholeJSON.get("username").asText();
+
+        UserCredentials userCredentials = userCredentialsRepository.findByUsername(username);
+        if (userCredentials==null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"User with username of "+username+" not found");
+        }
+        userCredentialsRepository.deleteById(userCredentials.getUserId());
+        return new ResponseEntity<>("User successfully deleted", HttpStatus.OK);
     }
 }
         
